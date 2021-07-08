74e53b114e0db943f69b9c4894735fc31b65b7f2
macnick <nick@macexperts.gr>
2021-07-08 11:12:58 +0300

    Store encrypted credentials SendGrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,16 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
-  config.action_mailer.delivery_method = :test
+  ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :authentication => :plain,
+    :user_name => Rails.application.credentials.dig(:user_name),
+    :password => Rails.application.credentials.dig(:password),
+    :domain => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+  config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.default_url_options = { :host => 'http://localhost:3000'}
   # Enable/disable caching. By default caching is disabled.
