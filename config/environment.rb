504ef4f4f6816d04e48f42472ac96d3607db2ef7
macnick <nick@macexperts.gr>
2021-07-21 13:49:25 +0300

    Use dig method to read the credentials properly from config/enviroment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,8 @@ require_relative "application"
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => Rails.application.credentials.sendgrid[:user_name],
-  :password => Rails.application.credentials.sendgrid[:password],
+  :user_name => Rails.application.credentials.dig(:sendgrid, :user_name),
+  :password => Rails.application.credentials.dig(:sendgrid, :password),
   :domain => 'heroku.com',
   :address => 'smtp.sendgrid.net',
   :port => '587',
