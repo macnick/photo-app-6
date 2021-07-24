f7b2ca91785d8279d946ba83b4ab72bb92dd86e4
macnick <nick@macexperts.gr>
2021-07-24 18:21:29 +0300

    Add stripe secret and publishable keys

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Stripe configuration using test keys
+  config.stripe.secret_key = Rails.application.credentials.stripe[:development][:secret_key]
+  config.stripe.publishable_key = Rails.application.credentials.stripe[:development][:publishable_key]
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -130,4 +130,8 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  # Stripe configuration using test keys
+  config.stripe.secret_key = Rails.application.credentials.stripe[:development][:secret_key]
+  config.stripe.publishable_key = Rails.application.credentials.stripe[:development][:publishable_key]
 end
