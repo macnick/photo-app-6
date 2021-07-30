013ed495a8758ffe45ce10a1384ac8685a2f24c3
macnick <nick@macexperts.gr>
2021-07-30 11:29:16 +0300

    Fix the sendgrid authentication error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   
   ActionMailer::Base.smtp_settings = {
   :user_name => 'apikey',
-  :password => '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',
+  :password => Rails.application.credentials.dig(:sendgrid, :password),
   :domain => 'heroku.com',
   :address => 'smtp.sendgrid.net',
   :port => '587',
